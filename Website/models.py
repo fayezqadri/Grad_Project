aed9b44cfed8aa33efd42a60fa4a4d5cb86bc456
Walid Amro <walidamr.miscellaneous@gmail.com>
2023-05-08 18:10:09 +0300

    replaced VID_RES env var with VID_HEIGHT & VID_WIDTH

--- Website/models.py
+++ Website/models.py
@@ -8,7 +8,8 @@ import requests
 CACHE_DYNAMODB_TABLE_NAME = os.environ.get('CACHE_DYNAMODB_TABLE_NAME')
 ML_API_DNS_NAME = os.environ.get('ML_API_DNS_NAME')
 ML_API_BASE_PATH = os.environ.get('ML_API_BASE_PATH', default="/api/v1/")
-VID_RES = tuple(map(int, os.environ.get('VID_RES', default='240,240').split(',')))
+VID_HEIGHT = int(os.environ.get('VID_HEIGHT', default=240))
+VID_WIDTH = int(os.environ.get('VID_WIDTH', default=240))
 VID_DURATION = int(os.environ.get('VID_DURATION', default=2))
 TOTAL_OUTPUT_FRAMES = int(os.environ.get('TOTAL_OUTPUT_FRAMES', default=25))
 
@@ -22,32 +23,18 @@ process = (
     .video
     .trim(duration=VID_DURATION)
     .filter('fps', fps=TOTAL_OUTPUT_FRAMES/VID_DURATION)
-    .filter('scale', f'{VID_RES[0]}x{VID_RES[1]}')
-    .filter('setsar', ratio=f'{VID_RES[0]}/{VID_RES[1]}')
+    .filter('scale', f'{VID_WIDTH}x{VID_HEIGHT}')
+    .filter('setsar', ratio=f'{VID_WIDTH}/{VID_HEIGHT}')
+    .trim(duration=VID_DURATION)
     .output('pipe:', format='rawvideo', pix_fmt='rgb24')
     # .run_async(pipe_stdin=True, pipe_stdout=True)
 )
 
 
-def get_vid_arr_from_bytes(video_bytes: bytes) -> "np.ndarray[np.uint8].shape[TOTAL_OUTPUT_FRAMES, VID_RES[1], VID_RES[0], 3]":
-    print(VID_RES)
-    print(VID_DURATION)
-    print(TOTAL_OUTPUT_FRAMES)
-    process = (
-        ffmpeg
-        .input('pipe:')
-        .video
-        .trim(duration=VID_DURATION)
-        .filter('fps', fps=TOTAL_OUTPUT_FRAMES/VID_DURATION)
-        .filter('scale', f'{VID_RES[0]}x{VID_RES[1]}')
-        .filter('setsar', ratio=f'{VID_RES[0]}/{VID_RES[1]}')
-        .trim(duration=VID_DURATION)
-        .output('pipe:', format='rawvideo', pix_fmt='rgb24')
-        # .run_async(pipe_stdin=True, pipe_stdout=True)
-    )
+def get_vid_arr_from_bytes(video_bytes: bytes) -> "np.ndarray[np.uint8].shape[TOTAL_OUTPUT_FRAMES, VID_HEIGHT, VID_WIDTH, 3]":
     try:
         vid_raw, _ = process.run(input=video_bytes, capture_stdout=True, capture_stderr=True)
-        vid_arr = np.frombuffer(vid_raw, np.uint8).reshape((TOTAL_OUTPUT_FRAMES, VID_RES[1], VID_RES[0], 3))
+        vid_arr = np.frombuffer(vid_raw, np.uint8).reshape((TOTAL_OUTPUT_FRAMES, VID_HEIGHT, VID_WIDTH, 3))
 
         return vid_arr
     except ffmpeg.Error as e:
@@ -55,7 +42,7 @@ def get_vid_arr_from_bytes(video_bytes: bytes) -> "np.ndarray[np.uint8].shape[TO
         print('stderr:', e.stderr.decode('utf8'))
         raise e
 
-def get_classification(vid_arr: "np.ndarray[np.uint8].shape[TOTAL_OUTPUT_FRAMES, VID_RES[1], VID_RES[0], 3]") -> str:
+def get_classification(vid_arr: "np.ndarray[np.uint8].shape[TOTAL_OUTPUT_FRAMES, VID_HEIGHT, VID_WIDTH, 3]") -> str:
     vid_arr_hash_hexdigest = hashlib.md5(vid_arr.tobytes()).hexdigest()
 
     # cached_response = DYNAMODB_CACHE_TABLE.get_item(Key={'hash_key': vid_arr_hash_hexdigest})
@@ -67,7 +54,7 @@ def get_classification(vid_arr: "np.ndarray[np.uint8].shape[TOTAL_OUTPUT_FRAMES,
     #     return pred
 
 
-def get_inference(vid_arr: "np.ndarray[np.uint8].shape[TOTAL_OUTPUT_FRAMES, VID_RES[1], VID_RES[0], 3]") -> str:
+def get_inference(vid_arr: "np.ndarray[np.uint8].shape[TOTAL_OUTPUT_FRAMES, VID_HEIGHT, VID_WIDTH, 3]") -> str:
     return 234
     # reponse = requests.post(ML_API_DNS_NAME+ML_API_BASE_PATH+'video-classification', data=vid_arr.tobytes())
     # return reponse.json()['predicted_class']
